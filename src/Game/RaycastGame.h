8d6a6cb75cb5cf7391bad3031201f6770f7c8e91
warzes <warzes@mail.ru>
2024-07-02 21:21:15 +1000

    - чистка

--- src/Game/RaycastGame.h
+++ src/Game/RaycastGame.h
@@ -751,119 +751,6 @@ void main()
 		std::vector<Sprite> sprites;
 		GLTexture2DRef texture;
 	};
-
-	class Buffer {
-	public:
-		enum Type {
-			ArrayBuffer,
-			ElementArrayBuffer,
-			ShaderStorageBuffer,
-		};
-
-		enum Usage {
-			StreamDraw,
-			StreamRead,
-			StreamCopy,
-			StaticDraw,
-			StaticRead,
-			StaticCopy,
-			DynamicDraw,
-			DynamicRead,
-			DynamicCopy,
-		};
-
-	private:
-		size_t bufferSize;
-		void* data = nullptr;
-		Type type;
-		Usage usage = StreamCopy;
-
-		void build();
-
-		uint32_t buffer = 0;
-
-	public:
-		Buffer(Type type, size_t size, Usage usage = StreamCopy) : type(type), usage(usage), bufferSize(size){
-		}
-		~Buffer();
-
-		void bind();
-		void bindBase(uint32_t index);
-		void setData(const void* data, size_t size);
-	};
-
-	constexpr int usageToGlUsage(Buffer::Usage usage) {
-		switch (usage) {
-		case Buffer::StreamDraw: return GL_STREAM_DRAW;
-		case Buffer::StreamRead: return GL_STREAM_READ;
-		case Buffer::StreamCopy: return GL_STREAM_COPY;
-		case Buffer::StaticDraw: return GL_STATIC_DRAW;
-		case Buffer::StaticRead: return GL_STATIC_READ;
-		case Buffer::StaticCopy: return GL_STATIC_COPY;
-		case Buffer::DynamicDraw: return GL_DYNAMIC_DRAW;
-		case Buffer::DynamicRead: return GL_DYNAMIC_READ;
-		case Buffer::DynamicCopy: return GL_DYNAMIC_COPY;
-		default: return GL_STREAM_COPY;
-		}
-	}
-
-	constexpr int typeToGlType(Buffer::Type type) {
-		switch (type) {
-		case Buffer::ArrayBuffer: return GL_ARRAY_BUFFER;
-		case Buffer::ElementArrayBuffer: return GL_ELEMENT_ARRAY_BUFFER;
-		case Buffer::ShaderStorageBuffer: return GL_SHADER_STORAGE_BUFFER;
-		default: return GL_ARRAY_BUFFER;
-		}
-	}
-
-	Buffer::~Buffer() {
-		if (buffer) {
-			glDeleteBuffers(1, &buffer);
-			buffer = 0;
-		}
-
-		if (data) {
-			free(data);
-			data = nullptr;
-		}
-	}
-
-	void Buffer::build() {
-		auto type = typeToGlType(this->type);
-		auto usage = usageToGlUsage(this->usage);
-		glGenBuffers(1, &buffer);
-		glBindBuffer(type, buffer);
-		glBufferData(type, bufferSize, data, usage);
-	}
-
-	void Buffer::bind() {
-		if (!buffer) {
-			build();
-		}
-
-		glBindBuffer(typeToGlType(type), buffer);
-	}
-
-	void Buffer::bindBase(uint32_t index) {
-		if (!buffer) {
-			build();
-		}
-
-		glBindBufferBase(typeToGlType(type), index, buffer);
-	}
-
-	void Buffer::setData(const void* data, size_t size) {
-		if (bufferSize < size || !this->data) {
-			this->data = realloc(this->data, size);
-		}
-
-		auto type = typeToGlType(this->type);
-		auto usage = usageToGlUsage(this->usage);
-		memcpy(this->data, data, size);
-		bufferSize = size;
-		glBufferData(type, bufferSize, data, usage);
-	}
-
 }
 
 void RaycastGame()
@@ -885,9 +772,9 @@ void RaycastGame()
 	glm::vec2 dir = currentMap->initialDir;
 	glm::vec2 plane = currentMap->initialPlane;
 
-	auto raycastResultBuffer = std::make_shared< GLShaderStorageBuffer>(10000 * (5 * sizeof(int32_t) + 5 * sizeof(float)));
-	 auto spritecastResultBuffer = std::make_shared< GLShaderStorageBuffer>(currentMap->sprites.size() * (8 * sizeof(int32_t) + 1 * sizeof(float) + 1 * sizeof(uint32_t)));
-	auto spritecastInputBuffer = std::make_shared< GLShaderStorageBuffer>(currentMap->sprites.size() * sizeof(raycast::Sprite), GL_DYNAMIC_COPY);
+	auto raycastResultBuffer = std::make_shared<GLShaderStorageBuffer>(10000 * (5 * sizeof(int32_t) + 5 * sizeof(float)));
+	 auto spritecastResultBuffer = std::make_shared<GLShaderStorageBuffer>(currentMap->sprites.size() * (8 * sizeof(int32_t) + 1 * sizeof(float) + 1 * sizeof(uint32_t)));
+	auto spritecastInputBuffer = std::make_shared<GLShaderStorageBuffer>(currentMap->sprites.size() * sizeof(raycast::Sprite), GL_DYNAMIC_COPY);
 
 	const std::vector<std::string_view> filepath{
 		"rc/textures/eagle.png",
@@ -974,6 +861,17 @@ void RaycastGame()
 			VAOEmpty->Bind();
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
+
+#pragma region imgui
+		IMGUI::Update();
+		{
+			ImGui::Begin((const char*)u8"Тест");
+			ImGui::Text((const char*)u8"Test/Тест/%s", u8"тест 2");
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0 / double(ImGui::GetIO().Framerate), double(ImGui::GetIO().Framerate));
+			ImGui::End();
+			IMGUI::Draw();
+		}
+#pragma endregion
 
 		Window::Swap();
 #pragma endregion
